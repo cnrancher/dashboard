ee7e5570c98a6a21f31fa9faeed1ad4e467c843d
LiuYan <361112237@qq.com>
2022-06-28 12:25:08 +0800

    feat(user-role): support read-only-pandaria role

--- shell/store/index.js
+++ shell/store/index.js
@@ -583,6 +583,10 @@ export const actions = {
       promises['workspaces'] = dispatch('management/findAll', { type: FLEET.WORKSPACE });
     }
 
+    if ( getters['management/schemaFor'](MANAGEMENT.GLOBAL_ROLE_BINDING) ) {
+      promises['globalRoleBindings'] = dispatch('management/findAll', { type: MANAGEMENT.GLOBAL_ROLE_BINDING });
+    }
+
     res = await allHash(promises);
 
     let isMultiCluster = true;
@@ -620,6 +624,19 @@ export const actions = {
         value: getters['prefs/get'](WORKSPACE),
         all:   res.workspaces,
       });
+    }
+
+    if (res.globalRoleBindings && getters['auth/me']?.id) {
+      const id = getters['auth/me']?.id;
+      const readOnlyAdmin = res.globalRoleBindings.find(binding => id === binding.userName && binding.globalRoleName === 'read-only-pandaria');
+
+      if (readOnlyAdmin) {
+        commit('auth/setReadOnlyAdmin', true);
+      } else {
+        commit('auth/setReadOnlyAdmin', false);
+      }
+    } else {
+      commit('auth/setReadOnlyAdmin', false);
     }
 
     console.log(`Done loading management; isRancher=${ isRancher }; isMultiCluster=${ isMultiCluster }`); // eslint-disable-line no-console
