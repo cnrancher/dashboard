2c9a26f95f666a949a693bb007c8e2596eb88888
LiuYan <361112237@qq.com>
2026-09-09 16:39:55 +0800

    Revert "fix(clusters): fix cluster table page stuttering"
    This reverts commit af02cca300702d468c2c8483af08d5adade625eb.

--- shell/models/management.cattle.io.cluster.js
+++ shell/models/management.cattle.io.cluster.js
@@ -511,8 +511,7 @@ export default class MgmtCluster extends SteveModel {
   }
 
   get nodes() {
-    // return this.$getters['all'](MANAGEMENT.NODE).filter((node) => node.id.startsWith(this.id));
-    return this.$getters['clusterIdToNodesMap'].get(this.id) ?? [];
+    return this.$getters['all'](MANAGEMENT.NODE).filter((node) => node.id.startsWith(this.id));
   }
 
   get nodesArchitecture() {
